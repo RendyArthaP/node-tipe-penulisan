95559e69fb47180b40625f494dfa77203e7f2551
Rendy Artha <rendyartha312@gmail.com>
2021-03-15 20:20:35 +0700

    async

--- async.js
+++ async.js
@@ -1,1 +1,19 @@
 // Penulisan node js dengan tipe asyncronous
+
+// Asynchronous
+
+const getUserAsync = (id, callback) => {
+  const time = id === 1 ? 1000 : 2000
+  setTimeout(() => {
+    const nama = id === 1 ? 'Rendy' : "Artha"
+    callback({ id, nama })
+  }, time)
+}
+
+const userSatu = getUserAsync(1, (hasil) => {
+  console.log(hasil)
+})
+
+const userDua = getUserAsync(2, (hasil) => {
+  console.log(hasil)
+})
